6415f84006cf1fb5bb441e23266dbaec45b6b1f9
Ekko <awdrg052@gmail.com>
2022-09-17 11:41:09 +0800

    初步更新
    AR還沒做

---
+++ Assets/Scripts/menu_btn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menu_btn : MonoBehaviour
+{
+    public void Triangle_Click()
+    {
+        SceneManager.LoadSceneAsync("AR_Triangle",LoadSceneMode.Single);
+        Debug.Log("t");
+    }
+    public void Circle_Click()
+    {
+        SceneManager.LoadSceneAsync("AR_Cirlce",LoadSceneMode.Single);
+        Debug.Log("c");
+    }
+    public void Rectangle_Click()
+    {
+        SceneManager.LoadSceneAsync("AR_Rectangle",LoadSceneMode.Single);
+        Debug.Log("r");
+    }
+    public void Test_Click()
+    {
+        Debug.Log("test!!");
+    }
+}
