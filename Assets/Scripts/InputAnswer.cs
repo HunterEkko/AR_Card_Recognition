b04be28ccdff137dbf8309500302ba80f5970eb6
Kevin0901 <j94191274@gmail.com>
2022-09-18 23:10:11 +0800

    輸入框程式大致OK

---
+++ Assets/Scripts/InputAnswer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class InputAnswer : MonoBehaviour
+{
+    private TMP_InputField Input;
+    [SerializeField] private string Answer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input = this.gameObject.GetComponent<TMP_InputField>();
+        Input.characterLimit = 8; 
+    }
+
+    public void Submit()
+    {
+        if(!Input.text.Equals(Answer))
+        {
+            Input.gameObject.GetComponentInChildren<TextMeshProUGUI>().color = Color.red;
+        }
+    }
+
+    public void ShowAnswer()
+    {
+        Input.text = Answer;
+        Input.gameObject.GetComponentInChildren<TextMeshProUGUI>().color = Color.green;
+    }
+}
--- Assets/Scripts/test_btn.cs
+++ Assets/Scripts/test_btn.cs
@@ -5,8 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class test_btn : MonoBehaviour
 {
+    [SerializeField] GameObject MainPage,TrianglePage,CirclePage,RectanglePage;
     public void backClick()
     {
-        SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Single);
+        if(MainPage.activeSelf)
+        {
+            SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Single);
+        }
+        else
+        {
+            TrianglePage.SetActive(false);
+            CirclePage.SetActive(false);
+            RectanglePage.SetActive(false);
+            MainPage.SetActive(true);
+        }
+    }
+    public void TriangleClick()
+    {
+        MainPage.SetActive(false);
+        TrianglePage.SetActive(true);
+    }
+    public void CircleClick()
+    {
+        MainPage.SetActive(false);
+        CirclePage.SetActive(true);
+    }
+    public void RectangleClick()
+    {
+        MainPage.SetActive(false);
+        RectanglePage.SetActive(true);
     }
 }
