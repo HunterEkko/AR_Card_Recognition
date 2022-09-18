030308db4d6812deaf3b8a4eb2fc85a03cfd7c2a
Ekko <awdrg052@gmail.com>
2022-09-18 15:25:49 +0800

    剩下測驗

---
+++ Assets/Scripts/test_btn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class test_btn : MonoBehaviour
+{
+    public void backClick()
+    {
+        SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Single);
+    }
+}
