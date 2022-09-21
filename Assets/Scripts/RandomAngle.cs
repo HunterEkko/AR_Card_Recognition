f9230a6e08a546e2ca8b632865c58683680aa651
Ekko <awdrg052@gmail.com>
2022-09-21 08:36:49 +0800

    圓形

---
+++ Assets/Scripts/RandomAngle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class RandomAngle : MonoBehaviour
+{
+    public GameObject blank;
+    private TMP_Text tmp;
+    private int[] arr = { 2, 3, 4, 5, 6, 8, 10, 12, 15, 18 };
+    private int denominator, numerator, ans;
+    void Start()
+    {
+        tmp = this.GetComponent<TMP_Text>();
+        generateAngle();
+    }
+    void generateAngle()
+    {
+        denominator = arr[Random.Range(0, arr.Length)];
+        numerator = Random.Range(1, denominator);
+        int k = gcd(denominator, numerator);
+        denominator /= k;
+        numerator /= k;
+        ans = 360 * numerator / denominator;
+        blank.GetComponent<InputAnswer>().SetLegel(ans);
+    }
+    int gcd(int a, int b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+        else
+        {
+            return gcd(b, a % b);
+        }
+    }
+}
