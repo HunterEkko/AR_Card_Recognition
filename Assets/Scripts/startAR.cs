70d889b275a1284058266f47b0e83afa5df52530
Ekko <awdrg052@gmail.com>
2022-09-28 02:02:31 +0800

    AR程式碼小更新

--- Assets/Scripts/startAR.cs
+++ Assets/Scripts/startAR.cs
@@ -4,6 +4,16 @@ public class startAR : MonoBehaviour
 {
     void Start()
     {
+        //實例化AR
         VuforiaRuntime.Instance.InitVuforia();
+    }
+
+    public void AR_open()
+    {
+        VuforiaBehaviour.Instance.enabled = true;
+    }
+    public void AR_close()
+    {
+        VuforiaBehaviour.Instance.enabled = false;
     }
 }
