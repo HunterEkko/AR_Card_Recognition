b2053fecbfd73a98b45be35d9ef7a073862fd3ae
Ekko <awdrg052@gmail.com>
2022-10-03 19:56:00 +0800

    放圖片上去

--- Assets/Scripts/ARManager.cs
+++ Assets/Scripts/ARManager.cs
@@ -2,21 +2,21 @@
 using Vuforia;
 public class ARManager : MonoBehaviour
 {
-    [SerializeField] private GameObject BackAR_Btn, Main_Btn;
+    // [SerializeField] private GameObject BackAR_Btn, Main_Btn;
     void Start()
     {
         //實例化AR
         VuforiaRuntime.Instance.InitVuforia();
     }
 
-    public void AR_open()
-    {
-        Main_Btn.SetActive(true);
-        BackAR_Btn.SetActive(false);
-    }
-    public void AR_close()
-    {
-        Main_Btn.SetActive(false);
-        BackAR_Btn.SetActive(true);
-    }
+    // public void AR_open()
+    // {
+    //     Main_Btn.SetActive(true);
+    //     BackAR_Btn.SetActive(false);
+    // }
+    // public void AR_close()
+    // {
+    //     Main_Btn.SetActive(false);
+    //     BackAR_Btn.SetActive(true);
+    // }
 }
--- Assets/Scripts/SingleUISwitch.cs
+++ Assets/Scripts/SingleUISwitch.cs
@@ -7,17 +7,11 @@ public class SingleUISwitch : MonoBehaviour
     public TrackableStatusManager TSM;
     public GameObject UI;
     private bool isOpenUI = false;
-    private GameObject AR;
-    void Start()
-    {
-        AR = GameObject.Find("ARManager");
-    }
     void Update()
     {
         if (TSM.Status == TrackableStatusManager.TurnStates.TRACK)
         {
             UI.SetActive(true);
-            AR.GetComponent<ARManager>().AR_close();
         }
         else
         {
